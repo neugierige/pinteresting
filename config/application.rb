a48c94bdc149cb0e252201863ddcc9d32d402a41
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-17 17:13:39 -0400

    removed heroku fix

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module Pinteresting
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # trying to fix heroku push error, "precompiling assets failed"
-    config.assets.initialize_on_precompile = false
-
   end
 end
